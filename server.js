7c3e91999ea4a421d3ff1664e00acc6a34ba55bc
Kitwana Akil <kakil@me.com>
2023-03-24 02:20:46 -0400

    Updated server.js to release version merged into main.

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json( { verify: rawBodySaver, extended: true }));
 mongoose.set('strictQuery', true);
 (async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+      await mongoose.connect(process.env.APPSETTING_MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
       console.log('Connected to MongoDB!!!');
     } catch (err) {
       console.error('Error connecting to MongoDB:', err);
@@ -33,7 +33,7 @@ mongoose.set('strictQuery', true);
   })();
     
 
-const port = process.env.PORT || 4242;
+const port = process.env.WEBSITES_PORT || 8080;
 app.use(express.json());
 
 //connect the routes
@@ -43,3 +43,16 @@ app.use('/api/stripe', require('./routes/stripe'));
 app.use(errorHandler);
 
 app.listen(port, () => { console.log(`Server is running on port ${port}`)});
+
+// set to production
+const node_env = process.env.APPSETTING_NODE_ENV;
+
+
+// renders the front end as a static application,
+// necessary for deployment and production
+if(node_env === 'production') {
+  app.use(express.static('./client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
